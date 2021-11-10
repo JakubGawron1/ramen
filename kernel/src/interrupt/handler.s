9e018b643affcc4c7a3bdab9d98ff5bbe710420e
Hiroki Tokunaga <tokusan441@gmail.com>
2021-11-10 19:55:39 +0900

    chore: destruct the interrupt handler macro
    These three handlers do different things, so we cannot integrate them.

--- kernel/src/interrupt/handler.s
+++ kernel/src/interrupt/handler.s
@@ -9,11 +9,10 @@
 .text
 .code64
 
-.macro  handler name
-.global \name\()_asm
-.extern \name
+.global h_20_asm
+.extern h_20
 
-\name\()_asm:
+h_20_asm:
 	push rbp
 	mov  rbp, rsp
 	push r15
@@ -32,7 +31,7 @@
 	push rax
 
 	mov  rsp, INTERRUPT_STACK
-	call \name
+	call h_20
 	mov  rsp, rax
 
 	pop rax
@@ -52,8 +51,89 @@
 
 	pop rbp
 	iretq
-	.endm
 
-	handler h_20
-	handler h_80
-	handler h_81
+	.global h_80_asm
+	.extern h_80
+
+h_80_asm:
+	push rbp
+	mov  rbp, rsp
+	push r15
+	push r14
+	push r13
+	push r12
+	push r11
+	push r10
+	push r9
+	push r8
+	push rdi
+	push rsi
+	push rdx
+	push rcx
+	push rbx
+	push rax
+
+	mov  rsp, INTERRUPT_STACK
+	call h_80
+	mov  rsp, rax
+
+	pop rax
+	pop rbx
+	pop rcx
+	pop rdx
+	pop rsi
+	pop rdi
+	pop r8
+	pop r9
+	pop r10
+	pop r11
+	pop r12
+	pop r13
+	pop r14
+	pop r15
+
+	pop rbp
+	iretq
+
+	.global h_81_asm
+	.extern h_81
+
+h_81_asm:
+	push rbp
+	mov  rbp, rsp
+	push r15
+	push r14
+	push r13
+	push r12
+	push r11
+	push r10
+	push r9
+	push r8
+	push rdi
+	push rsi
+	push rdx
+	push rcx
+	push rbx
+	push rax
+
+	mov  rsp, INTERRUPT_STACK
+	call h_81
+	mov  rsp, rax
+
+	pop rax
+	pop rbx
+	pop rcx
+	pop rdx
+	pop rsi
+	pop rdi
+	pop r8
+	pop r9
+	pop r10
+	pop r11
+	pop r12
+	pop r13
+	pop r14
+	pop r15
+
+	pop rbp
+	iretq
